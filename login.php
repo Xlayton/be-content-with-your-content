07a16d41f20039cc16cf476a82632b47cb129450
unknown <code-witch@users.noreply.github.com>
2020-08-14 16:30:32 -0600

    style changes

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
-
+<head>
+    <link href='./style.css' rel='stylesheet'>
+</head>
 <!-- The buttons to change session from either admin to user -->
 <form method="post" action="index.php" >
     <button type="submit" name="isAdmin" value ="1">Admin</button>
