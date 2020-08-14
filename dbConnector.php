860b5c08eb0bc5d8810e4e396d9b89aca4175781
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2020-08-13 19:37:48 -0600

    Changed port back to what it should be

--- dbConnector.php
+++ dbConnector.php
@@ -10,7 +10,7 @@ DEFINE ('DB_NAME', 'site_generator_db');
 function ConnGet() {
     // $dbConn will contain a resource link to the database
     // @ Don't display error
-    $dbConn = @mysqli_connect(DB_SERVER, DB_USER, DB_PSWD, DB_NAME, 5375)
+    $dbConn = @mysqli_connect(DB_SERVER, DB_USER, DB_PSWD, DB_NAME, 3308)
     OR die('Failed to connect to MySQL ' . DB_SERVER . '::' . DB_NAME . ' : ' . mysqli_connect_error()); // Display messge and end PHP script
 
     return $dbConn;
