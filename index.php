0093597b9997bfa1faa2a9e6fe7d33cd249424e3
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2020-08-13 21:27:14 -0600

    Tweaked code so parameter errors don't show up

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     require 'PageViewController.php';
 
     $databaseConnection = ConnGet();
-    $pageNum = (int)htmlspecialchars($_GET["pageNum"]) ? (int)htmlspecialchars($_GET["pageNum"]) : 1;
-    $isEdit = htmlspecialchars($_GET["isEdit"]) ? htmlspecialchars($_GET["isEdit"]) : false;
+    $pageNum = array_key_exists("pageNum", $_GET) ? (int)htmlspecialchars($_GET["pageNum"]) : 1;
+    $isEdit = array_key_exists("isEdit", $_GET) ? htmlspecialchars($_GET["isEdit"]) : false;
    
     $pageController = new PageViewController($pageNum, $databaseConnection, $pageNum);
     if($isEdit == "true") {
